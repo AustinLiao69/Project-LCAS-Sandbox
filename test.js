070127e0a51f262db2478e1ad19cb10a52837e91
Austin_Liao <austin.cn.liao@gmail.com>
2025-07-03 01:38:48 +0000

    OK

--- test.js
+++
@@ -1,4 +0,0 @@
-const { db } = require('./Modules/FB_Serviceaccountkey.js');
-db.collection('test').add({ ts: new Date(), msg: 'hello world' })
-  .then(() => console.log('OK'))
-  .catch(err => console.error('ERROR:', err));
